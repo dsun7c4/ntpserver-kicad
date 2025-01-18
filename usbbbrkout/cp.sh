a8c8af73928ada52d8659530774a7ae4be8b6e39
Daniel Sun <dsun7c4osh@gmail.com>
2025-01-18 15:47:26 -0500

    Update usbbbrkout design to KiCad 8.

--- usbbbrkout/cp.sh
+++ usbbbrkout/cp.sh
@@ -6,14 +6,14 @@ fi
 cp gerber/usbbbrkout-Edge_Cuts.gbr    oshpark/usbbbrkout.gko
 cp gerber/usbbbrkout.drl              oshpark/usbbbrkout.xln
 
-cp gerber/usbbbrkout-F_SilkS.gbr      oshpark/usbbbrkout.gto
+cp gerber/usbbbrkout-F_Silkscreen.gbr oshpark/usbbbrkout.gto
 cp gerber/usbbbrkout-F_Mask.gbr       oshpark/usbbbrkout.gts
 cp gerber/usbbbrkout-F_Cu.gbr         oshpark/usbbbrkout.gtl
 cp gerber/usbbbrkout-In1_Cu.gbr       oshpark/usbbbrkout.g2l
 cp gerber/usbbbrkout-In2_Cu.gbr       oshpark/usbbbrkout.g3l
 cp gerber/usbbbrkout-B_Cu.gbr         oshpark/usbbbrkout.gbl
 cp gerber/usbbbrkout-B_Mask.gbr       oshpark/usbbbrkout.gbs
-cp gerber/usbbbrkout-B_SilkS.gbr      oshpark/usbbbrkout.gbo
+cp gerber/usbbbrkout-B_Silkscreen.gbr oshpark/usbbbrkout.gbo
 
 rm oshpark.zip
 
