4fd04081a963caad4133cfb8e146c1c68d3cd825
Daniel Sun <dsun7c4osh@gmail.com>
2025-01-19 08:59:57 -0500

    Update usbabrkout design to KiCad 8.

--- usbabrkout/cp.sh
+++ usbabrkout/cp.sh
@@ -6,14 +6,14 @@ fi
 cp gerber/usbabrkout-Edge_Cuts.gbr    oshpark/usbabrkout.gko
 cp gerber/usbabrkout.drl              oshpark/usbabrkout.xln
 
-cp gerber/usbabrkout-F_SilkS.gbr      oshpark/usbabrkout.gto
+cp gerber/usbabrkout-F_Silkscreen.gbr oshpark/usbabrkout.gto
 cp gerber/usbabrkout-F_Mask.gbr       oshpark/usbabrkout.gts
 cp gerber/usbabrkout-F_Cu.gbr         oshpark/usbabrkout.gtl
 cp gerber/usbabrkout-In1_Cu.gbr       oshpark/usbabrkout.g2l
 cp gerber/usbabrkout-In2_Cu.gbr       oshpark/usbabrkout.g3l
 cp gerber/usbabrkout-B_Cu.gbr         oshpark/usbabrkout.gbl
 cp gerber/usbabrkout-B_Mask.gbr       oshpark/usbabrkout.gbs
-cp gerber/usbabrkout-B_SilkS.gbr      oshpark/usbabrkout.gbo
+cp gerber/usbabrkout-B_Silkscreen.gbr oshpark/usbabrkout.gbo
 
 rm oshpark.zip
 
