7761b7971e1a54ae9868f45800a1e2aeaf618c8d
Daniel Sun <dsun7c4osh@gmail.com>
2025-01-19 09:51:03 -0500

    Update rj45brkout design to KiCad 8.

--- rj45brkout/cp.sh
+++ rj45brkout/cp.sh
@@ -6,14 +6,14 @@ fi
 cp gerber/rj45brkout-Edge_Cuts.gbr    oshpark/rj45brkout.gko
 cp gerber/rj45brkout.drl              oshpark/rj45brkout.xln
 
-cp gerber/rj45brkout-F_SilkS.gbr      oshpark/rj45brkout.gto
+cp gerber/rj45brkout-F_Silkscreen.gbr oshpark/rj45brkout.gto
 cp gerber/rj45brkout-F_Mask.gbr       oshpark/rj45brkout.gts
 cp gerber/rj45brkout-F_Cu.gbr         oshpark/rj45brkout.gtl
 cp gerber/rj45brkout-In1_Cu.gbr       oshpark/rj45brkout.g2l
 cp gerber/rj45brkout-In2_Cu.gbr       oshpark/rj45brkout.g3l
 cp gerber/rj45brkout-B_Cu.gbr         oshpark/rj45brkout.gbl
 cp gerber/rj45brkout-B_Mask.gbr       oshpark/rj45brkout.gbs
-cp gerber/rj45brkout-B_SilkS.gbr      oshpark/rj45brkout.gbo
+cp gerber/rj45brkout-B_Silkscreen.gbr oshpark/rj45brkout.gbo
 
 rm oshpark.zip
 
