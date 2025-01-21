15b6e50b3154833ef2f021cf6aa6312b1b7a0fe8
Daniel Sun <dsun7c4osh@gmail.com>
2025-01-21 13:28:02 -0500

    Update term design to KiCad 8.

--- term/cp.sh
+++ term/cp.sh
@@ -7,8 +7,8 @@ cp gerber/term-F_Cu.gbr         oshpark/term.gtl
 cp gerber/term-B_Cu.gbr         oshpark/term.gbl
 cp gerber/term-F_Mask.gbr       oshpark/term.gts
 cp gerber/term-B_Mask.gbr       oshpark/term.gbs
-cp gerber/term-F_SilkS.gbr      oshpark/term.gto
-cp gerber/term-B_SilkS.gbr      oshpark/term.gbo
+cp gerber/term-F_Silkscreen.gbr oshpark/term.gto
+cp gerber/term-B_Silkscreen.gbr oshpark/term.gbo
 cp gerber/term-Edge_Cuts.gbr    oshpark/term.gko
 cp gerber/term.drl              oshpark/term.drl
 
