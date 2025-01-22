65407bfcf74b015b85ac1c7d52a6dbc92402192f
Daniel Sun <dsun7c4osh@gmail.com>
2025-01-22 17:57:51 -0500

    Update 8x16seg design to KiCad 8.

--- 8x16seg/cp.sh
+++ 8x16seg/cp.sh
@@ -6,14 +6,14 @@ fi
 cp gerber/8x16seg-Edge_Cuts.gbr    oshpark/8x16seg.gko
 cp gerber/8x16seg.drl              oshpark/8x16seg.xln
 
-cp gerber/8x16seg-F_SilkS.gbr      oshpark/8x16seg.gto
+cp gerber/8x16seg-F_Silkscreen.gbr oshpark/8x16seg.gto
 cp gerber/8x16seg-F_Mask.gbr       oshpark/8x16seg.gts
 cp gerber/8x16seg-F_Cu.gbr         oshpark/8x16seg.gtl
 cp gerber/8x16seg-In1_Cu.gbr       oshpark/8x16seg.g2l
 cp gerber/8x16seg-In2_Cu.gbr       oshpark/8x16seg.g3l
 cp gerber/8x16seg-B_Cu.gbr         oshpark/8x16seg.gbl
 cp gerber/8x16seg-B_Mask.gbr       oshpark/8x16seg.gbs
-cp gerber/8x16seg-B_SilkS.gbr      oshpark/8x16seg.gbo
+cp gerber/8x16seg-B_Silkscreen.gbr oshpark/8x16seg.gbo
 
 rm oshpark.zip
 
