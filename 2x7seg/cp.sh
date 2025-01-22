4349c472cb7ee29be2e84b44573d300aecc4b117
Daniel Sun <dsun7c4osh@gmail.com>
2025-01-22 10:39:16 -0500

    Update 2x7seg design to KiCad 8.

--- 2x7seg/cp.sh
+++ 2x7seg/cp.sh
@@ -7,8 +7,8 @@ cp gerber/disp-F_Cu.gbr         oshpark/2x7seg.gtl
 cp gerber/disp-B_Cu.gbr         oshpark/2x7seg.gbl
 cp gerber/disp-F_Mask.gbr       oshpark/2x7seg.gts
 cp gerber/disp-B_Mask.gbr       oshpark/2x7seg.gbs
-cp gerber/disp-F_SilkS.gbr      oshpark/2x7seg.gto
-cp gerber/disp-B_SilkS.gbr      oshpark/2x7seg.gbo
+cp gerber/disp-F_Silkscreen.gbr oshpark/2x7seg.gto
+cp gerber/disp-B_Silkscreen.gbr oshpark/2x7seg.gbo
 cp gerber/disp-Edge_Cuts.gbr    oshpark/2x7seg.gko
 cp gerber/disp.drl              oshpark/2x7seg.drl
 
