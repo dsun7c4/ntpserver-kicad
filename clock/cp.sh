8926eff8d9b56d965edda53f84f843bd62a07403
Daniel Sun <dsun7c4osh@gmail.com>
2025-01-24 10:34:19 -0500

    Update clock design to KiCad 8.

--- clock/cp.sh
+++ clock/cp.sh
@@ -7,8 +7,8 @@ cp gerber/clock-F_Cu.gbr         oshpark/clock.gtl
 cp gerber/clock-B_Cu.gbr         oshpark/clock.gbl
 cp gerber/clock-F_Mask.gbr       oshpark/clock.gts
 cp gerber/clock-B_Mask.gbr       oshpark/clock.gbs
-cp gerber/clock-F_SilkS.gbr      oshpark/clock.gto
-cp gerber/clock-B_SilkS.gbr      oshpark/clock.gbo
+cp gerber/clock-F_Silkscreen.gbr oshpark/clock.gto
+cp gerber/clock-B_Silkscreen.gbr oshpark/clock.gbo
 cp gerber/clock-Edge_Cuts.gbr    oshpark/clock.gko
 cp gerber/clock.drl              oshpark/clock.xln
 
